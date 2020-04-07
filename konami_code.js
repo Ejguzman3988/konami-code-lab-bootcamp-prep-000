06982e3d6d41af29f8d39a7afb69e8b04d564b4e
Eriberto Guzman <eriberto176@gmail.com>
2020-04-07 09:53:30 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,7 +15,23 @@ function init() {
   // your code here
   
   
-  document.body.addEventListener("keydown", function(event){
-    
+  let index = 0;
+  
+  document.body.addEventListener("keydown", function onKeyDownHandler(e){
+      const key = e.key;
+      
+      if (key === codes[index]){
+        index++;
+      
+      
+        if (index === codes.length){
+          alert("Hurray!");
+        
+          index = 0
+        }
+      }else {
+        index = 0;
+      }
+
   })
 }
